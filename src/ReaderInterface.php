fa78a8ffa22499223532f96198c9748f089d5922
proggeler <bas@dmt-software.nl>
2023-02-16 14:27:08 +0100

    added flags for xml or json reader handlers

--- src/ReaderInterface.php
+++ src/ReaderInterface.php
@@ -4,10 +4,17 @@ namespace DMT\Import\Reader;
 
 use Closure;
 use DMT\Import\Reader\Exceptions\ReaderReadException;
+use DMT\XmlParser\Parser;
+use DMT\XmlParser\Tokenizer;
 use Iterator;
+use pcrov\JsonReader\JsonReader;
 
 interface ReaderInterface
 {
+    public const JSON_FLOATS_AS_STRINGS = JsonReader::FLOATS_AS_STRINGS;
+    public const XML_DROP_NAMESPACES = Tokenizer::XML_DROP_NAMESPACES;
+    public const XML_USE_CDATA = Tokenizer::XML_USE_CDATA;
+
     /**
      * Read through a file.
      *
